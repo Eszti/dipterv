4b769e382de64f254e6e73d4f671cfc9ce41ccb9
Eszter <eszter.iklodi@gmail.com>
2017-11-16 23:49:57 +0100

    Modified embedding loading

--- panlex/check_valid_pairs.py
+++ panlex/check_valid_pairs.py
@@ -1,22 +1,23 @@
-import datetime
 import json
+import logging
 import subprocess
 
 import os
 import pandas as pd
-import logging
+
+from embedding import FasttextEmbedding
 
 sil2fb_fn = '/home/eszti/projects/dipterv/notebooks/panlex/data/sil2fb.json'
 
 with open(sil2fb_fn) as f:
      sil2fb = json.load(f)
 
-# langs = ['eng', 'spa', 'fra']
+# langs = ['spa', 'fra']
 langs = ['eng', 'hun', 'deu', 'ita', 'spa', 'fra']
 
 pan_fold = '6_lang'
 emb_fold = '/mnt/permanent/Language/Multi/FB'
-out_fold = 'temp_out'
+out_fold = '6_lang_out'
 loglevel = logging.INFO
 
 logging.basicConfig(filename=os.path.join(out_fold, 'log.txt'),
@@ -57,79 +58,60 @@ for lang_pair, df in df_dicts.items():
 logging.info('Tables for languages are created!')
 
 
+# Add found column
+for lang in df_langs_plx.keys():
+    df_langs_plx[lang] = df_langs_plx[lang].dropna()
+    df_langs_plx[lang] = df_langs_plx[lang].assign(found = False)
+
+# Language statistics
+header = ['lang', 'words', 'found']
+df_lang_stat = pd.DataFrame(columns = header)
+
+
 # Search for embeddings
-lookfor = None
-df_langs_emb = dict()
-df_langs_merged = dict()
 for sil in langs:
     fb = sil2fb[sil]
-    # Total number of words found in panlex data
-    tot = len(df_langs_plx[sil].index)
-    logging.info('total # of words: {}'.format(tot))
-    # Create df for words found in embedding
-    df_langs_emb[sil] = pd.DataFrame(columns=[sil])
+
     # Get embedding
-    emb_fn = os.path.join(emb_fold, 'wiki.{0}/wiki.{0}.vec'.format(fb))
-    logging.info('Reading embeddings from: {}'.format(emb_fn))
-    # Counting embedding lines
-    p = subprocess.Popen(['wc', '-l', emb_fn], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result, err = p.communicate()
-    if p.returncode != 0:
-        raise IOError(err)
-    lines = int(result.strip().split()[0])
-    logging.info('# lines in embed file: {}'.format(lines))
-    with open(emb_fn) as f:
-        i = 0
-        for line in f:
-            if i == 0:
-                i += 1
-                continue
-            if lookfor is not None and i > lookfor:
-                logging.info('Breaking, i ({}) = lookfor ({})'.format(i-1, lookfor))
-                break
-            fields = line.strip().split(' ')
-            w = fields[0]
-            w = w.lower()
-            row = pd.DataFrame([[w]], columns=[sil])
-            df_langs_emb[sil] = df_langs_emb[sil].append(row)
-            i+=1
-            if (i-1) % 10000 == 0:
-                ts = datetime.datetime.now()
-                logging.info('timestamp: {}\t# checked words:  {}'.format(ts, i-1))
-    # Match words encountered in the embedding
-    df_langs_emb[sil] = df_langs_emb[sil].assign(found = True)
-    df_langs_merged[sil] = df_langs_plx[sil].merge(df_langs_emb[sil], how='left')
-    df_langs_merged[sil] = df_langs_merged[sil].fillna(False)
-    logging.info('len plx: {0}'.format(len(df_langs_plx[sil].index)))
-    logging.info('len emb: {0}'.format(len(df_langs_emb[sil].index)))
-    logging.info('len merged: {}\tTrue: {}\tFalse: {}'
-          .format(len(df_langs_merged[sil].index),
-                  len(df_langs_merged[sil][df_langs_merged[sil]['found'] == True].index),
-                  len(df_langs_merged[sil][df_langs_merged[sil]['found'] == False].index)
-                  )
-          )
-    # Save found words
+    ext = 'bin'
+    emb_fn = os.path.join(emb_fold, 'wiki.{0}/wiki.{0}.{1}'.format(fb, ext))
+    if ext == 'vec':
+        p = subprocess.Popen(['wc', '-l', emb_fn], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, err = p.communicate()
+        if p.returncode != 0:
+            raise IOError(err)
+        lines = int(result.strip().split()[0])
+        logging.info('# lines in embed file: {}'.format(lines))
+    # Load embedding
+    emb = FasttextEmbedding(emb_fn)
+
+    # Checking if vector in embedding exists for the words
+    for i, row in df_langs_plx[sil].iterrows():
+        w = row[sil]
+        if w in emb.model:
+            df.loc[df[sil] == w, 'found'] = True
+
+    # Save and stat
+    tot = len(df_langs_plx[sil].index)
+    f = len(df_langs_plx[sil][df_langs_plx[sil]['found'] == True])
+    logging.info('# words: {}\t# found: {}'.format(tot, f))
     found_fn = os.path.join(out_fold, '{}_found.tsv'.format(sil))
-    df_langs_merged[sil].to_csv(found_fn, sep='\t', index=False)
+    df_langs_plx[sil].to_csv(found_fn, sep='\t', index=False)
+    stat_row = pd.DataFrame([[lang, tot, f]], columns=header)
+    df_lang_stat = df_lang_stat.append(stat_row)
+
 
 # Merge tables
 df_merged_dicts = dict()
 for lang_pair, df in df_dicts.items():
     lang1 = lang_pair[0]
     lang2 = lang_pair[1]
-    tmp1 = pd.merge(df, df_langs_merged[lang1], on=lang1)
-    tmp2 = pd.merge(tmp1, df_langs_merged[lang2], on=lang2)
+    tmp1 = pd.merge(df, df_langs_plx[lang1], on=lang1)
+    tmp2 = pd.merge(tmp1, df_langs_plx[lang2], on=lang2)
     df_merged_dicts[lang_pair] = tmp2
 
-# Language statistics
-header = ['lang', 'words', 'found']
-df_lang_stat = pd.DataFrame(columns = header)
-for lang, df in df_langs_merged.items():
-    l = len(df.index)
-    f = len(df[df['found'] == True])
-    row = pd.DataFrame([[lang, l, f]], columns = header)
-    df_lang_stat = df_lang_stat.append(row)
 
+# Save language statistics
 fn = os.path.join(out_fold, 'df_lang_stat.tsv')
 df_lang_stat.to_csv(fn, sep='\t', index=False)
 
